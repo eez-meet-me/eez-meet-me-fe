4c87e4aeaa9d8cff09ad3c6404e352dd889218c6
Zach Butler <zach.standley93@gmail.com>
2019-09-16 19:57:23 -0700

    stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+// import React from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch
+// } from 'react-router-dom';
+// import GoogleMap from '../components/containers/GoogleMap';
+// import { withSession } from '../Auth0Provider';
+
+// export default function App() {
+//   return (
+//     <>
+//       <Router>
+//         <Switch>
+//           <Route path="/" component={withSession(GoogleMap)}/>
+//         </Switch>
+//       </Router>
+//     </>
+//   );
+// }
+
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
-import GoogleMap from '../components/containers/GoogleMap';
-import { withSession } from '../Auth0Provider';
+
+import React from 'react';
+import Play from './play/Play';
+import Fun from './play/Fun';
 
 export default function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/" component={withSession(GoogleMap)}/>
+          <Route path="/" component={Play} />
+          <Route path="/fun" component={Fun}/>
         </Switch>
       </Router>
     </>
---
+++ src/components/play/Fun.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Fun() {
+  return (
+    <p>You did it!</p>
+  );
+}
+
+export default Fun;
