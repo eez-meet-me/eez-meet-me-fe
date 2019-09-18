eeaf5d45e79e92d3ceac36a5853f0257bb7c5d4c
Zach Butler <zach.standley93@gmail.com>
2019-09-18 10:22:48 -0700

    made basic header with some styles

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.css';
+
+function Header() {
+  return (
+    <section className={styles.Header}>
+      <h1>Pindrop</h1>
+    </section>
+  );
+}
+
+export default Header;
