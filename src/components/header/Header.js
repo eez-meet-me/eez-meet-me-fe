87c29e6733212d5ad2dfa29928df987560ca8250
Zach Butler <zach.standley93@gmail.com>
2019-09-18 10:43:42 -0700

    imported drawer into header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './Header.css';
+import Drawer from '../drawer/Drawer';
 
 function Header() {
   return (
     <section className={styles.Header}>
+      <Drawer />
       <h1>Pindrop</h1>
     </section>
   );
