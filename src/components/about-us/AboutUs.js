c9adb005a67149327744d3e2d729c9d0169fa813
Zach Butler <zach.standley93@gmail.com>
2019-09-18 11:49:31 -0700

    created aboutus and profile components and rendering them example wise in app

---
+++ src/components/about-us/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    // <section className={styles.AboutUs}>
+    <h1>About Us</h1>
+    // </section>
+  );
+}
+
+export default AboutUs;
