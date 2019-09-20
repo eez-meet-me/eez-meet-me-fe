eb2b470757fe26baa7142b5ade97e0ab1bb5c43b
Erin Gurley <erinmgurley@gmail.com>
2019-09-19 19:46:32 -0700

    About us changes

--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './AboutUs.css';
+import Header from '../header/Header';
 
 function AboutUs() {
   return (
-    <section className={styles.AboutUs}>
-      <h1>About Us</h1>
-    </section>
+    <div>
+      <Header />
+      <section className={styles.AboutUs}>
+        <h1>About Us</h1>
+      </section>
+      <section>
+        <div>Eli</div>
+        <div>Zach</div>
+        <div>Erin</div>
+      </section>
+    </div>
   );
 }
 
