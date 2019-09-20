865804cf4de0c081d8d22300485c18e6071963e6
Erin Gurley <erinmgurley@gmail.com>
2019-09-19 17:24:16 -0700

    list scroll feature working boiiiii!

--- src/components/markerList/MarkerList.js
+++ src/components/markerList/MarkerList.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MarkerListItem from './MarkerListItem';
 import styles from './MarkerList.css';
 
 function MarkerList({ pins }) {
   const pinElements = pins.map(pin => {
     return (
-      <td key={pin._id}>
-        <MarkerListItem pin={pin} />
-      </td>
+      <tr key={pin._id}>
+        <td> {pin.where}</td>
+        <td>{pin.address}</td>
+        <td>{pin.startTime}</td>
+        <td>{pin.endTime}</td>
+        <td>{pin.message}</td>
+      </tr>
     );
   });
 
   return (
-    <section id="wow" className={styles.MarkerList}>
+    <section className={styles.MarkerList}>
       <table>
-        {pinElements}
+        <thead>
+          <tr>
+            <th>Location</th>
+            <th>Address</th>
+            <th>Start Time</th>
+            <th>End Time</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pinElements}
+        </tbody>
       </table>
     </section>
   );
--- src/components/markerList/MarkerListItem.js
+++ src/components/markerList/MarkerListItem.js
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './MarkerListItem.css';
 import DeleteButtonContainer from '../containers/DeleteButtonContainer';
-function MarkerListItem({ pin }) {
 
+// Unused
+function MarkerListItem({ pin }) {
+  console.log(pin);
   return (
     <tr className={styles.MarkerListItem}> 
       <td> {pin.where}</td>
       <td>{pin.address}</td>
-      <td>{pin.startTime} - </td>
+      <td>{pin.startTime}</td>
       <td>{pin.endTime}</td>
       <td>{pin.message}</td>
       <DeleteButtonContainer pin={pin} />
