5ad884cfe63ca932c61e913396678b53ba10d606
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-09-19 18:30:36 -0700

    follower email

--- src/components/followersList/FollowersList.js
+++ src/components/followersList/FollowersList.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import FollowerListItem from './FollowerListItem';
 
 function FollowersList({ followers }) {
-  const followerElements = followers.map(follower => {
+  const followerElements = followers.map((follower, i) => {
     return (
-      <li key={follower._id}>
+      <li key={i}>
         <FollowerListItem follower={follower} />
       </li>
     );
